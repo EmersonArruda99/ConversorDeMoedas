28abc3ecf5d16cd010e09c222e59f78e754e5ffb
Emerson Arruda <emersonarruda753@gmail.com>
2024-05-13 20:46:51 -0300

    Finalizado opções para escolha.

--- src/PrincipalConversor.java
+++ src/PrincipalConversor.java
@@ -19,6 +19,10 @@ public class PrincipalConversor {
                     """
                         1 - Real(BRL) para Dolar(USD).
                         2 - Dolar(USD) para Real(BRL).
+                        3 - Peso argentino(ARS) para Real(BRL).
+                        4 - Real(BRL) para Peso argentino(ARS)
+                        5 - Yen japonês(JPY) para Real(BRL).
+                        6 - Euro(EUR) para Real(BRL).
                         7 - Fechar o programa.\s
                    \s""");
             System.out.print("Escolha uma opção: ");
@@ -40,16 +44,32 @@ public class PrincipalConversor {
                     mensagem.convertida("usd","brl",valor,novaMoeda);
                     break;
                 case 3:
-
+                    System.out.println("|*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*|\n");
+                    System.out.print(" Digite o valor que quer converter: ");
+                    valor = leitura.nextDouble();
+                    novaMoeda = moeda.BuscaMoeda("ars","brl",valor);
+                    mensagem.convertida("ars","brl",valor,novaMoeda);
                     break;
                 case 4:
-
+                    System.out.println("|*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*|\n");
+                    System.out.print(" Digite o valor que quer converter: ");
+                    valor = leitura.nextDouble();
+                    novaMoeda = moeda.BuscaMoeda("BRL","ars",valor);
+                    mensagem.convertida("brl","ars",valor,novaMoeda);
                     break;
                 case 5:
-
+                    System.out.println("|*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*|\n");
+                    System.out.print(" Digite o valor que quer converter: ");
+                    valor = leitura.nextDouble();
+                    novaMoeda = moeda.BuscaMoeda("jpy","brl",valor);
+                    mensagem.convertida("jpy","brl",valor,novaMoeda);
                     break;
                 case 6:
-
+                    System.out.println("|*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*|\n");
+                    System.out.print(" Digite o valor que quer converter: ");
+                    valor = leitura.nextDouble();
+                    novaMoeda = moeda.BuscaMoeda("eur","brl",valor);
+                    mensagem.convertida("eur","brl",valor,novaMoeda);
                     break;
                 case 7:
                     System.out.println("Programa fechando...");
